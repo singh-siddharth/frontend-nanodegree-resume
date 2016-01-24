ffee1649ec9ccd87f33677ea02e8073d1d9b15db
Siddharth Singh <siddharthsingh@iitj.ac.in>
2016-01-24 17:46:34 +0530

    addons

--- js/helper.js
+++ js/helper.js
@@ -1,8 +1,10 @@
 /*
 
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
+This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because
+ they support your code in this course.
 
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
+Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until
+you start experimenting with inserting a Google Map in Problem Set 3.
 
 Cameron Pittman
 */
@@ -13,12 +15,12 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span><b>%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile :</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<b><li class="flex-item"><span class="orange-text">TWITTER:</span><span class="white-text">%data%</span></li><br>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
@@ -32,6 +34,7 @@ var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
+
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,28 +1,39 @@
-var work = {};
-var projects={};
-var bio= {};
-var education={};
+//var work = {};
+//var projects={};
+var bio= {
+	"name" : "Siddharth Kumar Singh",
+	"role" : "B.Tech, Computer Science and Engineering, IIT JODHPUR",
+	"Contacts": {
+		"mobile":  '<br><b><li class="flex-item"><span class="orange-text">MOBILE :</span><span class="white-text">987-380-6518</span></li><br>',
+		"email" : '<b><li class="flex-item"><span class="orange-text">EMAIL  :</span><span class="white-text">siddharthsingh@iitj.ac.in</span></li><br>',
+		"github": '<b><li class="flex-item"><span class="orange-text">GITHUB :</span><span class="white-text">singh-siddharth</span></li><br>',
+		"twitter": HTMLtwitter.replace("%data%","@dilli_wala"),
+		"location": '<b><li class="flex-item"><span class="orange-text">LOCATION :</span><span class="white-text">New Delhi</span></li><br> </b>'
+	},
+	"welcoleMessage": " Happy Coding!!",
+	"skills" : [
+		"awesome", "coding","sleeping","quoracious","whatever"],
+	"bioPic": "images/J.jpg"
+};
+//var education={};
 
+var helpername = HTMLheaderName.replace("%data%","SIDDHARTH KUMAR SINGH");
+var helperrole = HTMLheaderRole.replace("%data%",bio.role);
+var bioPic = HTMLbioPic.replace("%data%",bio.bioPic);
+var tashan = HTMLskills.replace("%data%",bio.skills);
+$("#header").prepend(helperrole);
+$("#header").prepend(helpername);
+$("#header").append(bioPic);
+$("#topContacts").append(bio.Contacts.mobile);
+$("#topContacts").append(bio.Contacts.email);
+$("#topContacts").append(bio.Contacts.github);
+$("#topContacts").append(bio.Contacts.twitter);
+$("#topContacts").append(bio.Contacts.location);
+$("#header").append(HTMLskillsStart);
+for(skill in bio.skills)
+$("#header").append(HTMLskills.replace("%data%",bio.skills[skill]));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-$("#main").append("Siddharth singh");.
-
-This is empty on purpose! Your code to build the resume will go here.
- */
-/* $("#main").append("SKSbskksj");
+/* 
 var email ="siddharth@iitj.ac.in";
 var newmail = email.replace("iitj","gmail");
 console.log(email);
@@ -37,10 +48,7 @@ $("#main").append(some); */
 /*
 var name = "siddharth";
 var role = "coder";
-var helpername = HTMLheaderName.replace("%data%","SIDDHARTH KUMAR SINGH");
-var helperrole = HTMLheaderRole.replace("%data%","FULL STACK DEVELPOER");
-$("#header").append(helpername);
-$("#header").append(helperrole);
+
 var bio = {
 	"name" : "Siddharth",
 	"Contact" : "\n 9873806518 \n ",
